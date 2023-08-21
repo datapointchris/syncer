42282faae2bda08a876b9fcc1d2eb914a5745319
Chris Birch <datapointchris@gmail.com>
2023-08-21 01:21:40 -0500

    chore: Remove unused main command

--- syncer/main.py
+++ syncer/main.py
@@ -3,15 +3,6 @@ from syncer import dotfiles, projects, plugins, testpaths, update
 
 app = typer.Typer()
 
-
-@app.command()
-def main():
-    """
-    Sync dotfiles, projects, or plugins
-    """
-    pass
-
-
 app.add_typer(dotfiles.app, name='dotfiles')
 app.add_typer(projects.app, name='projects')
 app.add_typer(plugins.app, name='plugins')
