22ebf6bda2ad7929e9e1f9bfbb57e3a841c58c1d
Chris Birch <datapointchris@gmail.com>
2023-08-21 01:19:52 -0500

    feat: Use dynamic wheel path instead of hardcoded version

--- syncer/update.py
+++ syncer/update.py
@@ -1,23 +1,24 @@
-import pathlib
-import typer
 import os
 import subprocess
+
+import typer
 from rich import print
 
-app = typer.Typer()
+from .config import settings
 
-HARDCODED_BASE_DIR = pathlib.Path().home() / 'code' / 'syncer'
+app = typer.Typer()
 
 
 @app.callback(invoke_without_command=True)
 @app.command()
 def main():
     print('[blue]Updating syncer...[/blue]')
-    print(HARDCODED_BASE_DIR)
-    os.chdir(HARDCODED_BASE_DIR)
+    print(settings.syncer.BASE)
+    os.chdir(settings.syncer.BASE)
     print('[green]Building new wheel...[/green]')
     subprocess.call('deactivate', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     subprocess.call('poetry build', shell=True)
     print('[green]Installing new wheel...[/green]')
-    subprocess.call('pip install -q --user ~/code/syncer/dist/syncer-0.45.0-py3-none-any.whl --force-reinstall', shell=True)
+    wheel_path = next(settings.syncer.DIST_DIR.glob('*.whl'))
+    subprocess.call(f'pip install -q --user {wheel_path} --force-reinstall', shell=True)
     print('[green]Done![/green]')
