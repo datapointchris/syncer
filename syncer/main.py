d27f875e8a6cdf60ae572cb76140b0b7eb4f2245
Chris Birch <datapointchris@gmail.com>
2024-02-20 22:19:18 -0600

    refactor: rename projects to repos

--- syncer/main.py
+++ syncer/main.py
@@ -1,10 +1,20 @@
+import logging
+
 import typer
-from syncer import dotfiles, projects, plugins, testpaths, update
+
+from syncer import dotfiles, plugins, repos, testpaths, update
 
 app = typer.Typer()
 
+logger = logging.getLogger("__name__")
+handler = logging.FileHandler("/usr/local/var/log/syncer.log")
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
 app.add_typer(dotfiles.app, name='dotfiles')
-app.add_typer(projects.app, name='projects')
+app.add_typer(repos.app, name='repos')
 app.add_typer(plugins.app, name='plugins')
 app.add_typer(testpaths.app, name='testpaths')
 app.add_typer(update.app, name='update')
