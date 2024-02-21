27a9166bf4a8bc41522e7566a3fd88b230fc1869
Chris Birch <datapointchris@gmail.com>
2024-02-20 22:17:52 -0600

    refactor: split plugins and projects by type

--- syncer/config.py
+++ syncer/config.py
@@ -1,54 +1,49 @@
-from pydantic import BaseSettings
 import pathlib
+
+from pydantic_settings import BaseSettings
 
 
 class SyncerSettings(BaseSettings):
-    BASE: pathlib.Path = pathlib.Path().home() / 'code' / 'syncer'
-    DIST_DIR: pathlib.Path = BASE / 'dist'
+    ROOT: pathlib.Path = pathlib.Path.home() / 'code' / 'syncer'
 
 
 class DotfilesSettings(BaseSettings):
-    MAC_ONLY_DOTFILES: list[str] = ['.aws/config', '.aws/credentials', '.gitconfig']
-    LINUX_ONLY_DOTFILES: list[str] = ['.gitconfig-linux']
+    MAC_ONLY_DOTFILES: list[str] = [".aws/config", ".aws/credentials", ".gitconfig"]
+    LINUX_ONLY_DOTFILES: list[str] = [".gitconfig-linux"]
     UNIVERSAL_DOTFILES: list[str] = [
-        '.bash_profile',
-        '.bash_prompt',
-        '.zshrc',
-        '.bashrc',
-        '.inputrc',
-        '.shellcheckrc',
-        '.tmux.conf',
-        '.config/neofetch',
-        '.config/tmuxinator',
-        '.iterm2_shell_integration.zsh',
-        '.oh-my-zsh/custom/themes/datapointchris.zsh-theme',
+        ".bash_profile",
+        ".bash_prompt",
+        ".zshrc",
+        ".bashrc",
+        ".inputrc",
+        ".shellcheckrc",
+        ".tmux.conf",
+        ".config/neofetch",
+        ".config/tmuxinator",
+        ".iterm2_shell_integration.zsh",
+        ".oh-my-zsh/custom/themes/datapointchris.zsh-theme",
     ]
 
-    class DotfilesSourceSettings(BaseSettings):
-        BASE: pathlib.Path = pathlib.Path().home() / 'code' / 'dotfiles'
-        DOTFILES: pathlib.Path = BASE / 'dotfiles'
-        BINS: pathlib.Path = BASE / 'bin'
+    SOURCE_BASE: pathlib.Path = pathlib.Path().home() / "code" / "dotfiles"
+    SOURCE_DOTFILES: pathlib.Path = SOURCE_BASE / "dotfiles"
+    SOURCE_BINS: pathlib.Path = SOURCE_BASE / "bin"
 
-    class DotfilesTargetSettings(BaseSettings):
-        BASE: pathlib.Path = pathlib.Path().home()
-        DOTFILES: pathlib.Path = BASE
-        MAC_BINS: pathlib.Path = pathlib.Path('/usr/local/bin')
-
-    source = DotfilesSourceSettings()
-    target = DotfilesTargetSettings()
+    TARGET_BASE: pathlib.Path = pathlib.Path().home()
+    TARGET_DOTFILES: pathlib.Path = TARGET_BASE
+    TARGET_MAC_BINS: pathlib.Path = pathlib.Path("/usr/local/bin")
 
 
 class DataSettings(BaseSettings):
-    DATA_DIR: pathlib.Path = pathlib.Path(__file__).parent / 'data'
-    PLUGINS: pathlib.Path = DATA_DIR / 'plugins.json'
-    PROJECTS: pathlib.Path = DATA_DIR / 'projects.json'
-    LABS: pathlib.Path = DATA_DIR / '1904labs.json'
+    SYNCER_DATA_DIR: pathlib.Path = pathlib.Path(__file__).parent / "data"
+    PLUGINS_DIR: pathlib.Path = SYNCER_DATA_DIR / 'plugins'
+    REPOS_DIR: pathlib.Path = SYNCER_DATA_DIR / 'repos'
+    CODE_ROOT: pathlib.Path = pathlib.Path.home() / 'code'
 
 
 class Settings(BaseSettings):
-    syncer = SyncerSettings()
-    dotfiles = DotfilesSettings()
-    data = DataSettings()
+    syncer: SyncerSettings = SyncerSettings()
+    dotfiles: DotfilesSettings = DotfilesSettings()
+    data: DataSettings = DataSettings()
 
 
 settings = Settings()
