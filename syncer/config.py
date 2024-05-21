3c02fbb3fce014c79a4a9280c61bd6176c984295
Chris Birch <datapointchris@gmail.com>
2024-05-21 16:07:40 -0400

    fix: add .profile back in and include poetry lock

--- syncer/config.py
+++ syncer/config.py
@@ -8,33 +8,34 @@ class SyncerSettings(BaseSettings):
 
 
 class DotfilesSettings(BaseSettings):
-    MAC_ONLY_DOTFILES: list[str] = [".aws/config", ".aws/credentials", ".gitconfig"]
-    LINUX_ONLY_DOTFILES: list[str] = [".gitconfig-linux"]
+    MAC_ONLY_DOTFILES: list[str] = ['.aws/config', '.aws/credentials', '.gitconfig']
+    LINUX_ONLY_DOTFILES: list[str] = ['.gitconfig-linux']
     UNIVERSAL_DOTFILES: list[str] = [
-        ".bash_profile",
-        ".bash_prompt",
-        ".zshrc",
-        ".bashrc",
-        ".inputrc",
-        ".shellcheckrc",
-        ".tmux.conf",
-        ".config/neofetch",
-        ".config/tmuxinator",
-        ".iterm2_shell_integration.zsh",
-        ".oh-my-zsh/custom/themes/datapointchris.zsh-theme",
+        '.bash_profile',
+        '.bash_prompt',
+        '.zshrc',
+        '.bashrc',
+        '.inputrc',
+        '.profile',
+        '.shellcheckrc',
+        '.tmux.conf',
+        '.config/neofetch',
+        '.config/tmuxinator',
+        '.iterm2_shell_integration.zsh',
+        '.oh-my-zsh/custom/themes/datapointchris.zsh-theme',
     ]
 
-    SOURCE_BASE: Path = Path.home() / "code" / "dotfiles"
-    SOURCE_DOTFILES: Path = SOURCE_BASE / "dotfiles"
-    SOURCE_BINS: Path = SOURCE_BASE / "bin"
+    SOURCE_BASE: Path = Path.home() / 'code' / 'dotfiles'
+    SOURCE_DOTFILES: Path = SOURCE_BASE / 'dotfiles'
+    SOURCE_BINS: Path = SOURCE_BASE / 'bin'
 
     TARGET_BASE: Path = Path.home()
     TARGET_DOTFILES: Path = TARGET_BASE
-    TARGET_MAC_BINS: Path = Path("/usr/local/bin")
+    TARGET_MAC_BINS: Path = Path('/usr/local/bin')
 
 
 class DataSettings(BaseSettings):
-    SYNCER_DATA_DIR: Path = Path(__file__).parent / "data"
+    SYNCER_DATA_DIR: Path = Path(__file__).parent / 'data'
     PLUGINS_DIR: Path = SYNCER_DATA_DIR / 'plugins'
     REPOS_DIR: Path = SYNCER_DATA_DIR / 'repos'
     CODE_ROOT: Path = Path.home() / 'code'
